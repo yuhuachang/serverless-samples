405e90d736e319f2bd5114263ecbeb39ab33a19d
Yu-Hua Chang <yuhua_chang@morrisonexpress.com>
2018-04-20 17:52:37 +0800

    java sqs

--- sqs-java/src/main/java/com/serverless/Handler.java
+++ sqs-java/src/main/java/com/serverless/Handler.java
@@ -5,17 +5,70 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import com.amazon.sqs.javamessaging.AmazonSQSExtendedClient;
+import com.amazon.sqs.javamessaging.ExtendedClientConfiguration;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
 	private static final Logger LOG = Logger.getLogger(Handler.class);
 
+    // Default AWS Region
+    public static final Regions DEFAULT_REGION = Regions.US_WEST_2;
+
+    // S3 bucket name (need to create first.)
+    public static final String S3_BUCKET_NAME = "mec-edi-data";
+
+    // SQS name (standard queue is enough. need to create first.)
+    public static final String QUEUE_NAME = "dev-demo-test";
+    
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: " + input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+	    Response responseBody = null;
+		try {
+		    LOG.info("received: " + input);
+
+		    //TODO: retrieve payload
+	        String payload = "Hello World!";
+	        
+	        // Create S3 client
+	        final AmazonS3ClientBuilder s3ClientBuilder = AmazonS3ClientBuilder.standard().withRegion(DEFAULT_REGION);
+	        final AmazonS3 s3 = s3ClientBuilder.build();
+	        
+	        /*
+	         * Set the Amazon SQS extended client configuration with large payload
+	         * support enabled.
+	         */
+	        final ExtendedClientConfiguration extendedClientConfig = new ExtendedClientConfiguration()
+	                .withLargePayloadSupportEnabled(s3, S3_BUCKET_NAME).withAlwaysThroughS3(true);
+	        
+	        // Create SQS client
+	        final AmazonSQSClientBuilder sqsClientBuilder = AmazonSQSClientBuilder.standard().withRegion(DEFAULT_REGION);
+	        final AmazonSQS sqsExtended = new AmazonSQSExtendedClient(sqsClientBuilder.build(), extendedClientConfig);
+
+	        //TODO: retrieve queue url
+	        String queueUrl = "https://sqs.us-west-2.amazonaws.com/101654863091/dev-demo-test";
+
+	        // Send the message.
+	        final SendMessageRequest myMessageRequest = new SendMessageRequest(queueUrl, payload);
+	        SendMessageResult result = sqsExtended.sendMessage(myMessageRequest);
+	        String messageId = result.getMessageId();
+
+	        LOG.info("Sent the message.  MessageId = " + messageId);
+	        
+	        responseBody = new Response("Message was put to queue successfully!", input);
+		} catch (Throwable e) {
+		    responseBody = new Response("There is an error while putting message to the queue!", input, e);
+		}
+
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
--- sqs-java/src/main/java/com/serverless/Response.java
+++ sqs-java/src/main/java/com/serverless/Response.java
@@ -6,11 +6,19 @@ public class Response {
 
 	private final String message;
 	private final Map<String, Object> input;
+	private final Throwable e;
 
 	public Response(String message, Map<String, Object> input) {
 		this.message = message;
 		this.input = input;
+		this.e = null;
 	}
+	
+	public Response(String message, Map<String, Object> input, Throwable e) {
+        this.message = message;
+        this.input = input;
+        this.e = e;
+    }
 
 	public String getMessage() {
 		return this.message;
@@ -19,4 +27,8 @@ public class Response {
 	public Map<String, Object> getInput() {
 		return this.input;
 	}
+	
+	public Throwable getError() {
+        return this.e;
+    }
 }
